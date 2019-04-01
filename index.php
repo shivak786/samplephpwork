573a949b3edd995ec51b8e75f2b8b6db5034b9f0
shivak786 <42024419+shivak786@users.noreply.github.com>
2019-04-02 00:09:47 +0530

    Approval commit

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <h1>i'm a devops guru, i alwayz do the right job!</h1>
 <h1>i'm a devops guru, i alwayz do the right job!</h1>
 <h1>i'm a devops guru, i alwayz do the right job, testing!</h1>
+<h1>i'm a devops guru, i alwayz do the right job, checkking for approval!</h1>
 
 <?php
 echo "version :Ver 1.0 - checking";
