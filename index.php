7079c519683a908fc0f29a46a4890a6bf2c7d7d6
shivak786 <42024419+shivak786@users.noreply.github.com>
2019-04-02 00:04:33 +0530

    updated the code

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <h1>i'm a devops guru, i alwayz do the right job!</h1>
 <h1>i'm a devops guru, i alwayz do the right job!</h1>
+<h1>i'm a devops guru, i alwayz do the right job, testing!</h1>
 
 <?php
 echo "version :Ver 1.0 - checking";
